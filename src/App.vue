832aed048f555ff5611c8d9fe0f6030bf3e5c576
Denis Brester <49287397+DenKick@users.noreply.github.com>
2021-09-01 16:39:59 +0300

    Add Joke component and render the array of jokes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="main-container">
     <div v-if="isLoaded">
-      <span>Загрузилось</span>
+      <Joke v-for="joke in jokes"
+            :key="joke.id"
+            :id="joke.id"
+            :type="joke.type"
+            :text="joke.joke"
+            :setup="joke.setup"
+            :delivery="joke.delivery"
+            :category="joke.category"
+      />
     </div>
     <div class="loading-container" v-else>
       <Spinner />
@@ -11,24 +19,31 @@
 
 <script>
 import Spinner from './components/Spinner.vue';
+import Joke from './components/Joke.vue';
 
 export default {
   name: 'App',
   components: {
     Spinner,
+    Joke
   },
   methods: {
     apiFetch: function() {
       fetch('https://v2.jokeapi.dev/joke/Any?amount=10')
         .then(response => response.json())
-        .then(data => {this.apiResponse = data; /*this.isLoaded = true*/})
+        .then(data => {this.error = data.error;
+                       this.amount = data.amount;
+                       this.jokes = data.jokes;
+                       this.isLoaded = true})
         .catch(error => console.log(error));
     }
   },
   data: function() {
     return {
       isLoaded: false,
-      apiResponse: null,
+      error: null,
+      amount: null,
+      jokes: null,
     }
   },
   created() {
@@ -45,6 +60,7 @@ export default {
 }
 
 .main-container {
+  color: #4b6584;
   margin: 10% auto;
   height: 100%;
   width: 50vw;
