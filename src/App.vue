fc93d34ec81569260924f8a59173cef7d3a95b52
Denis Brester <49287397+DenKick@users.noreply.github.com>
2021-09-01 19:29:19 +0300

    Add search methods

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="main-container">
+    <div>
+      <input type="text" v-model='keyword'>
+    </div>
     <div v-if="isLoaded">
-      <Joke v-for="joke in jokes"
+      <Joke v-for="joke in filteredJokes"
             :key="joke.id"
             :id="joke.id"
             :type="joke.type"
@@ -36,6 +39,13 @@ export default {
                        this.jokes = data.jokes;
                        this.isLoaded = true})
         .catch(error => console.log(error));
+    },
+    filterJoke: function(joke) {
+      if (joke.type === 'single') {
+        return joke.joke.includes(this.keyword);
+      } else {
+        return joke.setup.includes(this.keyword) || joke.setup.includes(this.keyword);
+      }
     }
   },
   data: function() {
@@ -44,26 +54,30 @@ export default {
       error: null,
       amount: null,
       jokes: null,
+      keyword: '',
     }
   },
   created() {
     this.apiFetch();
+  },
+  computed: {
+    filteredJokes() {
+      return this.jokes.filter(joke => this.filterJoke(joke))
+    }
   }
 }
 </script>
-
+ 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-
 .main-container {
   color: #4b6584;
   margin: 10% auto;
   height: 100%;
   width: 50vw;
+
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 }
 
 .loading-container {
